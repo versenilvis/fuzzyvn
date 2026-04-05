393d337eb115974a3d7e6806553a74dd22cc0aee
verse91 <versedev.store@proton.me>
2026-04-05 22:56:17 +0700

    perf(utils): optimize normalization

--- core/utils.go
+++ core/utils.go
@@ -76,8 +76,8 @@ func CountWordMatches(queryWords []string, target string) int {
 }
 
 func Normalize(s string) string {
-	// 1. FAST PATH: Nếu toàn là ASCII (Tiếng Anh, Code) -> Lowercase và trả về ngay
-	// Đây là trường hợp phổ biến nhất (90% file source code) -> Tốc độ siêu nhanh
+	// Nếu toàn là ASCII (Tiếng Anh, Code) -> Lowercase và trả về ngay
+	// Đây là trường hợp phổ biến nhất
 	isASCII := true
 	for i := 0; i < len(s); i++ {
 		if s[i] > 127 {
@@ -89,45 +89,209 @@ func Normalize(s string) string {
 		return strings.ToLower(s)
 	}
 
-	// 2. NFD CHECK: Chỉ convert về NFC nếu chuỗi đang ở dạng NFD (thường gặp trên macOS)
-	// Hàm IsNormalString rất nhanh, giúp tránh việc allocate lại chuỗi nếu không cần thiết
+	// MacOS dùng NFD nên cần check
 	if !norm.NFC.IsNormalString(s) {
 		s = norm.NFC.String(s)
 	}
 
-	// 3. BUFFER: dùng []byte trực tiếp, nhanh hơn strings.Builder
+	// Duyệt trực tiếp trên byte, không decode rune
+	// Ký tự tiếng Việt NFC nằm trong phạm vi 2-3 byte UTF-8:
+	//   2-byte: 0xC0-0xDF + 1 byte tiếp theo (á, à, â, đ, é, ê, ...)
+	//   3-byte: 0xE0-0xEF + 2 byte tiếp theo (ắ, ằ, ẳ, ớ, ờ, ợ, ...)
 	buf := make([]byte, 0, len(s))
+	src := []byte(s)
+	i := 0
 
-	// 4. MANUAL MAPPING: Duyệt từng rune và map thủ công
-	for _, r := range s {
-		r = unicode.ToLower(r)
+	for i < len(src) {
+		b := src[i]
 
-		switch r {
-		case 'á', 'à', 'ả', 'ã', 'ạ', 'ă', 'ắ', 'ằ', 'ẳ', 'ẵ', 'ặ', 'â', 'ấ', 'ầ', 'ẩ', 'ẫ', 'ậ':
-			buf = append(buf, 'a')
-		case 'đ':
-			buf = append(buf, 'd')
-		case 'é', 'è', 'ẻ', 'ẽ', 'ẹ', 'ê', 'ế', 'ề', 'ể', 'ễ', 'ệ':
-			buf = append(buf, 'e')
-		case 'í', 'ì', 'ỉ', 'ĩ', 'ị':
-			buf = append(buf, 'i')
-		case 'ó', 'ò', 'ỏ', 'õ', 'ọ', 'ô', 'ố', 'ồ', 'ổ', 'ỗ', 'ộ', 'ơ', 'ớ', 'ờ', 'ở', 'ỡ', 'ợ':
-			buf = append(buf, 'o')
-		case 'ú', 'ù', 'ủ', 'ũ', 'ụ', 'ư', 'ứ', 'ừ', 'ử', 'ữ', 'ự':
-			buf = append(buf, 'u')
-		case 'ý', 'ỳ', 'ỷ', 'ỹ', 'ỵ':
-			buf = append(buf, 'y')
-		default:
-			if r < 128 {
-				buf = append(buf, byte(r))
-			} else if unicode.IsLetter(r) || unicode.IsDigit(r) {
+		// xử lý trực tiếp không cần decode
+		if b < 128 {
+			if b >= 'A' && b <= 'Z' {
+				buf = append(buf, b+32) // toLower
+			} else {
+				buf = append(buf, b)
+			}
+			i++
+			continue
+		}
+
+		// 2-byte UTF-8 sequence: 0xC0-0xDF
+		if b >= 0xC0 && b <= 0xDF && i+1 < len(src) {
+			b2 := src[i+1]
+			mapped := mapViet2Byte(b, b2)
+			if mapped != 0 {
+				buf = append(buf, mapped)
+				i += 2
+				continue
+			}
+			// không phải tiếng Việt -> giữ nguyên 2 byte (lowercase bằng unicode)
+			r, size := utf8.DecodeRune(src[i:])
+			r = unicode.ToLower(r)
+			if unicode.IsLetter(r) || unicode.IsDigit(r) {
 				var tmp [4]byte
 				n := utf8.EncodeRune(tmp[:], r)
 				buf = append(buf, tmp[:n]...)
 			}
+			i += size
+			continue
+		}
+
+		// 3-byte UTF-8 sequence: 0xE0-0xEF
+		if b >= 0xE0 && b <= 0xEF && i+2 < len(src) {
+			b2 := src[i+1]
+			b3 := src[i+2]
+			mapped := mapViet3Byte(b, b2, b3)
+			if mapped != 0 {
+				buf = append(buf, mapped)
+				i += 3
+				continue
+			}
+			// không phải tiếng Việt -> giữ nguyên
+			r, size := utf8.DecodeRune(src[i:])
+			r = unicode.ToLower(r)
+			if unicode.IsLetter(r) || unicode.IsDigit(r) {
+				var tmp [4]byte
+				n := utf8.EncodeRune(tmp[:], r)
+				buf = append(buf, tmp[:n]...)
+			}
+			i += size
+			continue
+		}
+
+		// 4-byte hoặc invalid -> decode bình thường
+		r, size := utf8.DecodeRune(src[i:])
+		r = unicode.ToLower(r)
+		if unicode.IsLetter(r) || unicode.IsDigit(r) {
+			var tmp [4]byte
+			n := utf8.EncodeRune(tmp[:], r)
+			buf = append(buf, tmp[:n]...)
+		}
+		i += size
+	}
+
+	return string(buf)
+}
+
+// mapViet2Byte: map 2-byte UTF-8 Vietnamese chars -> ASCII
+// trả về 0 nếu không phải ký tự Việt cần map
+func mapViet2Byte(b1, b2 byte) byte {
+	switch b1 {
+	case 0xC3:
+		// lowercase: à=0xA0, á=0xA1, â=0xA2, ã=0xA3
+		// è=0xA8, é=0xA9, ê=0xAA
+		// ì=0xAC, í=0xAD
+		// ò=0xB2, ó=0xB3, ô=0xB4, õ=0xB5
+		// ù=0xB9, ú=0xBA
+		// ý=0xBD
+		// uppercase: À=0x80, Á=0x81, Â=0x82, Ã=0x83
+		// È=0x88, É=0x89, Ê=0x8A
+		// Ì=0x8C, Í=0x8D
+		// Ò=0x92, Ó=0x93, Ô=0x94, Õ=0x95
+		// Ù=0x99, Ú=0x9A
+		// Ý=0x9D
+		switch b2 {
+		case 0x80, 0x81, 0x82, 0x83, 0xA0, 0xA1, 0xA2, 0xA3:
+			return 'a'
+		case 0x88, 0x89, 0x8A, 0xA8, 0xA9, 0xAA:
+			return 'e'
+		case 0x8C, 0x8D, 0xAC, 0xAD:
+			return 'i'
+		case 0x92, 0x93, 0x94, 0x95, 0xB2, 0xB3, 0xB4, 0xB5:
+			return 'o'
+		case 0x99, 0x9A, 0xB9, 0xBA:
+			return 'u'
+		case 0x9D, 0xBD:
+			return 'y'
+		}
+	case 0xC4:
+		// Đ=0x90, đ=0x91
+		if b2 == 0x90 || b2 == 0x91 {
+			return 'd'
+		}
+		// Ă=0x82, ă=0x83
+		if b2 == 0x82 || b2 == 0x83 {
+			return 'a'
+		}
+	case 0xC6:
+		// Ơ=0xA0, ơ=0xA1
+		if b2 == 0xA0 || b2 == 0xA1 {
+			return 'o'
+		}
+		// Ư=0xAF, ư=0xB0
+		if b2 == 0xAF || b2 == 0xB0 {
+			return 'u'
 		}
 	}
-	return string(buf)
+	return 0
+}
+
+// mapViet3Byte: map 3-byte UTF-8 Vietnamese chars -> ASCII
+// bao gồm các ký tự có dấu nặng: ắ, ằ, ẳ, ẵ, ặ, ấ, ầ, ẩ, ẫ, ậ ...
+func mapViet3Byte(b1, b2, b3 byte) byte {
+	if b1 != 0xE1 {
+		return 0
+	}
+
+	switch b2 {
+	case 0xBA:
+		// 0xE1 0xBA 0x80-0xBF
+		// ả=A2/A3, ạ=A0/A1, ấ=A4/A5, ầ=A6/A7, ẩ=A8/A9, ẫ=AA/AB, ậ=AC/AD
+		// ắ=AE/AF, ằ=B0/B1, ẳ=B2/B3, ẵ=B4/B5, ặ=B6/B7
+		// ẹ=B8/B9, ẻ=BA/BB, ẽ=BC/BD, ế=BE/BF
+		if b3 >= 0xA0 && b3 <= 0xB7 {
+			return 'a'
+		}
+		if b3 >= 0xB8 && b3 <= 0xBF {
+			return 'e'
+		}
+		// Ả=0x82, ả=0x83, Ạ=0x80, ạ=0x81...
+		if b3 >= 0x80 && b3 <= 0x9F {
+			// 80-87 = a variants, 88-8D = e variants, 8E-93 = i? check
+			// thực tế Unicode block 1EA0-1EBF:
+			// 1EA0=Ạ, 1EA1=ạ, 1EA2=Ả, 1EA3=ả, 1EA4=Ấ, 1EA5=ấ, 1EA6=Ầ, 1EA7=ầ
+			// 1EA8=Ẩ, 1EA9=ẩ, 1EAA=Ẫ, 1EAB=ẫ, 1EAC=Ậ, 1EAD=ậ, 1EAE=Ắ, 1EAF=ắ
+			// 1EB0=Ằ, 1EB1=ằ, 1EB2=Ẳ, 1EB3=ẳ, 1EB4=Ẵ, 1EB5=ẵ, 1EB6=Ặ, 1EB7=ặ
+			// 1EB8=Ẹ, 1EB9=ẹ, 1EBA=Ẻ, 1EBB=ẻ, 1EBC=Ẽ, 1EBD=ẽ, 1EBE=Ế, 1EBF=ế
+			// UTF-8 encoding: 0xE1 0xBA 0x80-0x9F maps to U+1E80-1E9F
+			// nhưng U+1E80-1E9F là Ẁ, ẁ, Ẃ, ẃ (Welsh W variants) không phải tiếng Việt
+			return 0
+		}
+	case 0xBB:
+		// 0xE1 0xBB 0x80-0xBF
+		// Unicode U+1EC0-1EFF
+		// 1EC0=Ề, 1EC1=ề, 1EC2=Ể, 1EC3=ể, 1EC4=Ễ, 1EC5=ễ, 1EC6=Ệ, 1EC7=ệ -> e
+		// 1EC8=Ỉ, 1EC9=ỉ, 1ECA=Ị, 1ECB=ị -> i
+		// 1ECC=Ọ, 1ECD=ọ, 1ECE=Ỏ, 1ECF=ỏ, 1ED0=Ố, 1ED1=ố, 1ED2=Ồ, 1ED3=ồ -> o
+		// 1ED4=Ổ, 1ED5=ổ, 1ED6=Ỗ, 1ED7=ỗ, 1ED8=Ộ, 1ED9=ộ -> o
+		// 1EDA=Ớ, 1EDB=ớ, 1EDC=Ờ, 1EDD=ờ, 1EDE=Ở, 1EDF=ở, 1EE0=Ỡ, 1EE1=ỡ, 1EE2=Ợ, 1EE3=ợ -> o
+		// 1EE4=Ụ, 1EE5=ụ, 1EE6=Ủ, 1EE7=ủ, 1EE8=Ứ, 1EE9=ứ -> u
+		// 1EEA=Ừ, 1EEB=ừ, 1EEC=Ử, 1EED=ử, 1EEE=Ữ, 1EEF=ữ, 1EF0=Ự, 1EF1=ự -> u
+		// 1EF2=Ỳ, 1EF3=ỳ, 1EF4=Ỵ, 1EF5=ỵ, 1EF6=Ỷ, 1EF7=ỷ, 1EF8=Ỹ, 1EF9=ỹ -> y
+		// UTF-8: b3 = codepoint & 0x3F | 0x80
+		// U+1EC0 -> 0xE1 0xBB 0x80, U+1EC7 -> 0xE1 0xBB 0x87
+		// e: 0x80-0x87
+		if b3 >= 0x80 && b3 <= 0x87 {
+			return 'e'
+		}
+		// i: 0x88-0x8B
+		if b3 >= 0x88 && b3 <= 0x8B {
+			return 'i'
+		}
+		// o: 0x8C-0xA3
+		if b3 >= 0x8C && b3 <= 0xA3 {
+			return 'o'
+		}
+		// u: 0xA4-0xB1
+		if b3 >= 0xA4 && b3 <= 0xB1 {
+			return 'u'
+		}
+		// y: 0xB2-0xB9
+		if b3 >= 0xB2 && b3 <= 0xB9 {
+			return 'y'
+		}
+	}
+	return 0
 }
 
 func FastSubstring(s string, n int) string {
